7e1a15f96d9f13db6483a8d0c61465988e7eedf6
Sei Yukinari <sei.yukinari@nextremer.com>
2023-12-07 10:28:27 +0900

    add subtract

--- src/lib.rs
+++ src/lib.rs
@@ -1,11 +1,22 @@
 /// Adds two numbers together.
 /// # Examples
 /// ```
+/// use arithmetic_yukinari::add;
 /// let a = add(1, 2);
 /// assert_eq!(a, 3);
 /// ```
 pub fn add(a: i32, b: i32) -> i32 {
     a + b
+}
+/// Subtracts two numbers.
+/// # Examples
+/// ```
+/// use arithmetic_yukinari::subtract;
+/// let s = subtract(1, 2);
+/// assert_eq!(s, -1);
+/// ```
+pub fn subtract(a: i32, b: i32) -> i32 {
+    a - b
 }
 
 #[cfg(test)]
@@ -13,7 +24,11 @@ mod tests {
     use super::*;
 
     #[test]
-    fn add_positive_numbers() {
+    fn add_numbers() {
         assert_eq!(add(2, 3), 5);
+    }
+    #[test]
+    fn subtract_numbers() {
+        assert_eq!(subtract(2, 3), -1);
     }
 }
